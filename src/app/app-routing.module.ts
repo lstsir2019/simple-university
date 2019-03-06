b77dbd5fc87e1c902fcce3df39f60db8d4ccfcf5
Ali <arabat50@gmail.com>
2019-03-06 16:43:18 +0100

    budget commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import {MagasinComponent} from "./magasin/magasin.component";
 import {ReceptionComponent} from "./reception/reception.component";
 import {ExpressionBesoinsComponent} from "./expression-besoins/expression-besoins.component";
+import {BudgetsComponent} from './budgets/budgets.component';
 
 const routes: Routes = [
   { path: 'exepressionbesoins', component:ExpressionBesoinsComponent},
   { path:'reception',component:ReceptionComponent},
-  { path: 'magasin',component:MagasinComponent}
-
+  { path: 'magasin',component:MagasinComponent},
+  { path: 'budget',component:BudgetsComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,16 @@ import {MagasinComponent} from "./magasin/magasin.component";
 import {MagasinCreateComponent} from "./magasin/magasin-create/magasin-create.component";
 import {StockListComponent} from "./magasin/stock-list/stock-list.component";
 import {StockUpdateComponent} from "./magasin/stock-update/stock-update.component";
+/*import {BudgetsComponent} from './budgets/budgets.component';
+import {BudgetFaculteComponent} from './budgets/budget-faculte/budget-faculte.component';
+import {BudgetEntiteAdministratifComponent} from './budgets/budget-entite-administratif/budget-entite-administratif.component';
+import {BudgetCompteBudgitaireComponent} from './budgets/budget-compte-budgitaire/budget-compte-budgitaire.component';
+import {BudgetSousProjetComponent} from './budgets/budget-sous-projet/budget-sous-projet.component';
+import {BfCreateComponent} from './budgets/budget-faculte/bf-create/bf-create.component';
+import {BspCreateComponent} from './budgets/budget-sous-projet/bsp-create/bsp-create.component';
+import {BeaCreateComponent} from './budgets/budget-entite-administratif/bea-create/bea-create.component';
+import {BcbCreateComponent} from './budgets/budget-compte-budgitaire/bcb-create/bcb-create.component';
+import {ConfirmationComponent} from './budgets/confirmation/confirmation.component';*/
 
 @NgModule({
   declarations: [
@@ -30,7 +40,17 @@ import {StockUpdateComponent} from "./magasin/stock-update/stock-update.componen
     MagasinComponent,
     MagasinCreateComponent,
     StockUpdateComponent,
-    StockListComponent
+    StockListComponent,
+    /*BudgetsComponent,
+    BudgetFaculteComponent,
+    BudgetSousProjetComponent,
+    BudgetEntiteAdministratifComponent,
+    BudgetCompteBudgitaireComponent,
+    BfCreateComponent,
+    BspCreateComponent,
+    BeaCreateComponent,
+    BcbCreateComponent,
+    ConfirmationComponent*/
   ],
   imports: [
     BrowserModule,
