18927856cdb6951b4a9ad30515163291530def39
mohcineaitellattare <mohcineaitellattare@gmail.com>
2019-03-08 17:54:52 +0100

    commande

---
+++ src/app/commandes/commandes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-commandes',
+  templateUrl: './commandes.component.html',
+  styleUrls: ['./commandes.component.css']
+})
+export class CommandesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
