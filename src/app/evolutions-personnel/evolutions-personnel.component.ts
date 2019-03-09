331491d1d06e1a3bfe7feb46e512ca83813c69a2
Elattar Saad <elattar.saad.3@gmail.com>
2019-03-09 13:05:14 +0100

    Evolution personnel added

---
+++ src/app/evolutions-personnel/evolutions-personnel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-evolutions-personnel',
+  templateUrl: './evolutions-personnel.component.html',
+  styleUrls: ['./evolutions-personnel.component.css']
+})
+export class EvolutionsPersonnelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/lois-evolution/loi-evolution-create/loi-evolution-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loi-evolution-create',
+  templateUrl: './loi-evolution-create.component.html',
+  styleUrls: ['./loi-evolution-create.component.css']
+})
+export class LoiEvolutionCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
