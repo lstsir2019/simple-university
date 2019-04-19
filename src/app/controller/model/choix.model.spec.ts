35fcb6a33f2e628bfb7151763a1aab210c353ec2
Elmassafi <anas.elmassa@gmail.com>
2019-04-19 11:39:22 +0100

    mohe

--- src/app/controller/model/choix.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Choix } from './choix.model';
-
-describe('Choix', () => {
-  it('should create an instance', () => {
-    expect(new Choix()).toBeTruthy();
-  });
-});
--- src/app/controller/model/concours.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Concours } from './concours.model';
-
-describe('Concours', () => {
-  it('should create an instance', () => {
-    expect(new Concours()).toBeTruthy();
-  });
-});
