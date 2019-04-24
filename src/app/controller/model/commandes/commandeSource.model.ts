4c61f48eedc7144511fd67e868cf876f69414834
ismailboulaanait <boulaanait.ismail@gmail.com>
2019-04-24 11:36:17 +0200

    add++

--- src/app/controller/model/commandes/commandeSource.model.ts
+++ src/app/controller/model/commandes/commandeSource.model.ts
@@ -3,5 +3,5 @@ import {CommandeItem} from './commande-item.model';
 
 export class CommandeSource {
   public commandeItemVo:CommandeItem=new CommandeItem('',0,0,Number(''));
-  constructor(public qteAffecte:number,public referenceExpressionBesoinItem:string){}
+  constructor(public qteAffecte:number,public referenceExpressionBesoinItem:string,public id:number){}
 }
