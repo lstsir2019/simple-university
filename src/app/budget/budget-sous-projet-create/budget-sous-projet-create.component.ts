1b3304322db3a8a5fb05a90700cd13377a967d74
oussamaFst <g.oussamaww2@gmail.com>
2019-05-21 03:22:29 +0000

    pull

--- src/app/budget/budget-sous-projet-create/budget-sous-projet-create.component.ts
+++ src/app/budget/budget-sous-projet-create/budget-sous-projet-create.component.ts
@@ -12,12 +12,22 @@ export class BudgetSousProjetCreateComponent implements OnInit {
 
   ngOnInit() {
   }
+  public mode:number=0;
 
   public get budgetSousProjetCreate(){
     return this.budgetService.budgetSousProjetCreate;
   }
   public ajouterNewBudgetSousProjet(){
     this.budgetService.ajouterNewBudgetSousProjet();
+  }
+
+
+  public changeMode(){
+    if (this.mode==0){
+      this.mode=1;
+    }else if (this.mode==1){
+      this.mode=0
+    }
   }
 
 }
