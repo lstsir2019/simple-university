1041f8c3ccf1ffa45c04b75c0021bcf13d51e50a
abdou <abdelouahedghouat1@gmail.com>
2019-05-22 14:51:12 +0000

    Mandat

---
+++ src/app/faculte-mandat/entite-administratifs/list-entite-administartif/list-entite-administartif.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-entite-administartif',
+  templateUrl: './list-entite-administartif.component.html',
+  styleUrls: ['./list-entite-administartif.component.css']
+})
+export class ListEntiteAdministartifComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
